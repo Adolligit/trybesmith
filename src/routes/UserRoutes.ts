95c74622c0a90cce8d33205861eee26b1091ef9f
Adolligit <adelson.onod@gmail.com>
2022-09-27 20:03:22 -0300

    feat: (User) [POST, '/'] foi criado um middleware de validação para os recebido pela requisição

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,9 +1,10 @@
 import express, { Express } from 'express';
 import UserController from '../controller/UserController';
+import UserValidation from '../middlewares/UserValidate';
 
 const user: Express = express();
 
-user.post('/', UserController.create);
+user.post('/', UserValidation, UserController.create);
 // user.get('/', UserController.all);
 
 export default user;
