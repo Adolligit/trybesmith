15f5764a68c318e589211879658422a56889b004
Adolligit <adelson.onod@gmail.com>
2022-09-27 20:18:54 -0300

    chore: (Product) tirando rota GET

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -5,6 +5,5 @@ import UserValidation from '../middlewares/UserValidate';
 const user: Express = express();
 
 user.post('/', UserValidation, UserController.create);
-// user.get('/', UserController.all);
 
 export default user;
