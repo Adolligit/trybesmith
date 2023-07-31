bb331090028d02f3df7a33a0d870cf86e00e432d
Adolligit <adelson.onod@gmail.com>
2023-07-31 01:05:47 -0300

    feat: (connection.ts) inserindo novos detalhes de conexão

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -5,6 +5,8 @@ dotenv.config();
 
 const connection: Pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
+  database: process.env.MYSQL_NAME,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
 });
