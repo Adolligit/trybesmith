293c8d3dc7136693a1469d0f62728c0c116c12cc
Adolligit <adelson.onod@gmail.com>
2023-08-16 17:22:09 -0300

    feat: (TokenValidate) inclui uma condição para evitar erro de validação do token

--- src/middlewares/TokenValidate.ts
+++ src/middlewares/TokenValidate.ts
@@ -5,12 +5,22 @@ import GenerateToken from '../utils/GenerateToken';
 export default function TokenValidation(req: Request, res: Response, next: NextFunction) {
   const { authorization } = req.headers;
 
-  if (!authorization) {
+  const token = (
+    authorization?.includes('Bearer') 
+      ? authorization.replace('Bearer', '').trim() 
+      : authorization
+  );
+
+  console.log('-->', token);
+
+  if (!token) {
     return res.status(httpStatus.UNAUTHORIZED).json({ message: 'Token not found' });
   }
 
   try {
-    const decode = GenerateToken.validate(authorization);
+    const decode = GenerateToken.validate(token);
+
+    console.log('decode', decode);
 
     res.locals.decode = decode;
   } catch (error) {
